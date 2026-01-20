7e7699b4203a162b9c03f7007804855b852277b1
megacysia <marcelina.nocon783@gmail.com>
2026-01-20 16:36:38 +0100

    changes

---
+++ cs50P/Lecture3/notes/cat.py
@@ -0,0 +1,24 @@
+#i = 0
+#while i < 3:
+#    print("meow")
+#    i += 1
+
+#list -> 
+
+#for i in[0, 1, 2]: #to w [] to lista
+#    print("meow")
+# 
+#no mozna tak robic ale co jak bede chciala milion elementow w tej liscie
+
+#zamiast tego moza tak:
+
+#for _ in range(3): #--> to but not through - zaczyna od 0
+#    print("meow")
+
+# ta podloga _ to jest oznaczenie zmienej ktorej poza tym ze jej potrzebuje do np tutaj fora, nigdy nie bede potrzebowac    
+
+#print("meow" * 3) #po prostu. pythonowo.
+
+#a jeszcze ladniej:
+
+print("meow\n" * 3, end="")
