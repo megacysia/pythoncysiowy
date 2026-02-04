c13946f112507630aeb11ab6a7075f8b95ad1686
megacysia <marcelina.nocon783@gmail.com>
2026-02-04 22:33:37 +0100

    daily

--- cs50P/Lecture4/problemset3/fuel.py
+++ cs50P/Lecture4/problemset3/fuel.py
@@ -3,3 +3,46 @@
 #In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, wherein X is a non-negative integer and Y is a positive integer, and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. And if 99% or more remains, output F instead to indicate that the tank is essentially full.
 
 #If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
+
+def main():
+    x = get_int()
+    y = give_percent(x)
+    if y <= 1:
+        print("Fuel is empty!")
+    elif y >= 99:
+        print("Fuel is full!")
+    else:
+        print(f"fuel is {y}%")
+
+
+def give_percent(wynik):
+    procenty = wynik * 100
+    return round(procenty) 
+
+
+def get_int():
+    while True:
+        try:
+            x = input("What's fuel fraction? ")
+            first, second = x.split("/")
+            liczebnik = int(first)
+            mianownik = int(second)
+            
+            if liczebnik >= 0 and mianownik >= 0 and liczebnik < mianownik:
+                return liczebnik / mianownik
+            if liczebnik > mianownik:
+                raise Exception("too much")
+            
+        except Exception:
+            print("You can't have THAT much! :D ")
+        
+        except ZeroDivisionError:
+            print("Can't divide by 0")
+
+        except ValueError:
+            print("It is not a fraction format")
+            
+        else: 
+            break
+    
+main()
--- cs50P/Lecture4/problemset3/grocery.py
+++ cs50P/Lecture4/problemset3/grocery.py
@@ -1,3 +1,21 @@
 # Suppose that you’re in the habit of making a list of items you need from the grocery store.
 
 # In a file called grocery.py, implement a program that prompts the user for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). Then output the user’s grocery list in all uppercase, sorted alphabetically by item, prefixing each line with the number of times the user inputted that item. No need to pluralize the items. Treat the user’s input case-insensitively.
+
+def main():
+    groceries = get_need()
+    groceries.sort()
+    print("List:", sep='\n')
+    for i in range (len(groceries)):
+        print(i+1, groceries[i], sep='. ')
+        
+def get_need():
+    list = []
+    while True:
+        try:
+            x = input("").upper()
+            list.append(x)
+        except EOFError:
+            return list
+            
+main()
