d46ca3ad55076f0e6783450bf9484852b7521ab7
megacysia <marcelina.nocon783@gmail.com>
2026-02-07 21:17:48 +0100

    daily

---
+++ cs50P/Lecture5/notes/generate.py
@@ -0,0 +1,23 @@
+#import random
+
+#coin = random.choice(["heads", "tails"])
+#print(coin)
+
+
+#albo:
+
+#from random import choice
+
+#coin = choice(["heads", "tails"])
+#print(coin)
+
+#import random
+#number = random.randint(1, 10)
+#print(number)
+
+
+import random
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
